d1bc0f9408034ddbcf12e053895416dc9c882c64
Martin Pecka <peckama2@fel.cvut.cz>
2023-06-07 18:00:46 +0200

    Rebase from 'release/noetic/sensor_filters'

--- include/sensor_filters/FilterChainBase.h
+++ include/sensor_filters/FilterChainBase.h
@@ -1,96 +1,84 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-FileCopyrightText: Czech Technical University in Prague
+
 #pragma once
-/*
- * Copyright (c) 2021, Czech Technical University in Prague
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Willow Garage, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+
+/**
+ * \file
+ * \brief Base for all sensor filter chains.
  */
+
+#include <string>
 
 #include <ros/ros.h>
 
 #if ROS_VERSION_MINIMUM(1, 15, 0)
 #include <filters/filter_chain.hpp>
 #else
+
 #include <filters/filter_chain.h>
+
 #endif
 
 
 namespace sensor_filters
 {
 
-template <typename T>
+template<typename T>
 class FilterChainBase
 {
-  protected: ros::Subscriber inputSubscriber;
-  protected: ros::Publisher outputPublisher;
-  protected: ros::NodeHandle topicNodeHandle;
-  protected: size_t inputQueueSize {10u};
-  protected: size_t outputQueueSize {10u};
-  protected: bool useSharedPtrMessages {true};
+protected:
+  ros::Subscriber inputSubscriber;
+  ros::Publisher outputPublisher;
+  ros::NodeHandle topicNodeHandle;
+  size_t inputQueueSize{10u};
+  size_t outputQueueSize{10u};
+  bool useSharedPtrMessages{true};
 
-  protected: filters::FilterChain<T> filterChain;
-  protected: T msg;
+  filters::FilterChain<T> filterChain;
+  T msg;
 
-  protected: typedef ros::message_traits::DataType<T> DataType;
+  typedef ros::message_traits::DataType<T> DataType;
 
-  public: FilterChainBase() :
+public:
+  FilterChainBase() :
     filterChain(std::string(DataType::value()).replace(std::string(DataType::value()).find('/'), 1, "::"))
   {
   }
 
-  public: virtual ~FilterChainBase() = default;
+  virtual ~FilterChainBase() = default;
 
-  protected: virtual void initFilters(
+protected:
+  virtual void initFilters(
     const std::string& filterChainNamespace, ros::NodeHandle filterNodeHandle, ros::NodeHandle topicNodeHandle,
     const bool useSharedPtrMessages, const size_t inputQueueSize, const size_t outputQueueSize)
   {
     if (!this->filterChain.configure(filterChainNamespace, filterNodeHandle))
     {
       ROS_ERROR_STREAM("Configuration of filter chain for "
-        << DataType::value() << " is invalid, the chain will not be run.");
+                         << DataType::value() << " is invalid, the chain will not be run.");
       throw std::runtime_error("Filter configuration error");
     }
 
-    ROS_INFO_STREAM("Configured filter chain of type " << DataType::value() <<
-      " from namespace " << filterNodeHandle.getNamespace() << "/" << filterChainNamespace);
+    ROS_INFO_STREAM("Configured filter chain of type " << DataType::value() << " from namespace "
+                                                       << filterNodeHandle.getNamespace() << "/"
+                                                       << filterChainNamespace);
 
     this->topicNodeHandle = topicNodeHandle;
     this->outputQueueSize = outputQueueSize;
     this->inputQueueSize = inputQueueSize;
     this->useSharedPtrMessages = useSharedPtrMessages;
-    
+
     this->advertise();
     this->subscribe();
   }
 
-  protected: virtual void advertise()
+  virtual void advertise()
   {
     this->outputPublisher = this->topicNodeHandle.template advertise<T>("output", this->outputQueueSize);
   }
 
-  protected: virtual void subscribe()
+  virtual void subscribe()
   {
     if (this->useSharedPtrMessages)
       this->inputSubscriber = this->topicNodeHandle.subscribe(
@@ -100,36 +88,36 @@ class FilterChainBase
         "input", this->inputQueueSize, &FilterChainBase::callbackReference, this);
   }
 
-  protected: virtual void publishShared(const typename T::ConstPtr& msg)
+  virtual void publishShared(const typename T::ConstPtr& msg)
   {
-      this->outputPublisher.publish(msg);
+    this->outputPublisher.publish(msg);
   }
 
-  protected: virtual void publishReference(const T& msg)
+  virtual void publishReference(const T& msg)
   {
-      this->outputPublisher.publish(msg);
+    this->outputPublisher.publish(msg);
   }
 
-  protected: virtual void callbackShared(const typename T::ConstPtr& msgIn)
+  virtual void callbackShared(const typename T::ConstPtr& msgIn)
   {
     typename T::Ptr msgOut(new T);
     if (this->filter(*msgIn, *msgOut))
       this->publishShared(msgOut);
   }
 
-  protected: virtual void callbackReference(const T& msgIn)
+  virtual void callbackReference(const T& msgIn)
   {
     if (this->filter(msgIn, this->msg))
       this->publishReference(this->msg);
   }
 
-  protected: virtual bool filter(const T& msgIn, T& msgOut)
+  virtual bool filter(const T& msgIn, T& msgOut)
   {
     ros::WallTime start = ros::WallTime::now();
     if (!this->filterChain.update(msgIn, msgOut))
     {
       ROS_ERROR_THROTTLE(1, "Filtering data from time %i.%i failed.",
-        msgIn.header.stamp.sec, msgIn.header.stamp.nsec);
+                         msgIn.header.stamp.sec, msgIn.header.stamp.nsec);
       return false;
     }
     ROS_DEBUG_STREAM("Filtering took " << (ros::WallTime::now() - start).toSec() << " s.");
