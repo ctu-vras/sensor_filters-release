428c2881088307ade45133885476eab088a018c6
Martin Pecka <peckama2@fel.cvut.cz>
2023-06-07 17:54:56 +0200

    Rebase from 'release/melodic/sensor_filters'

--- src/ImageFilterChainBase.cc
+++ src/ImageFilterChainBase.cc
@@ -1,31 +1,6 @@
-/*
- * Copyright (c) 2021, Czech Technical University in Prague
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Willow Garage, Inc. nor the names of its
- *       contributors may be used to endorse or promote products derived from
- *       this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-FileCopyrightText: Czech Technical University in Prague
+
 #include <memory>
 #include <string>
 
@@ -39,7 +14,7 @@
 namespace sensor_filters
 {
 
-void ImageFilterChainBase::initFilters(const std::string &filterChainNamespace, ros::NodeHandle filterNodeHandle,
+void ImageFilterChainBase::initFilters(const std::string& filterChainNamespace, ros::NodeHandle filterNodeHandle,
                                        ros::NodeHandle topicNodeHandle, const bool useSharedPtrMessages,
                                        const size_t inputQueueSize, const size_t outputQueueSize)
 {
@@ -48,18 +23,21 @@ void ImageFilterChainBase::initFilters(const std::string &filterChainNamespace, 
                                inputQueueSize, outputQueueSize);
 }
 
-void ImageFilterChainBase::advertise() {
+void ImageFilterChainBase::advertise()
+{
   const auto resolvedOutput = this->topicNodeHandle.resolveName("output");
   this->itPublisher = this->it->advertise(resolvedOutput, this->outputQueueSize);
 }
 
-void ImageFilterChainBase::subscribe() {
+void ImageFilterChainBase::subscribe()
+{
   const auto resolvedInput = this->topicNodeHandle.resolveName("input");
   this->itSubscriber = this->it->subscribe<ImageFilterChainBase>(
       resolvedInput, this->inputQueueSize, &ImageFilterChainBase::callbackShared, this);
 }
 
-void ImageFilterChainBase::publishShared(const sensor_msgs::ImageConstPtr &msg) {
+void ImageFilterChainBase::publishShared(const sensor_msgs::ImageConstPtr& msg)
+{
   this->itPublisher.publish(msg);
 }
 
